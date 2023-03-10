30ed0cd0086f2c2cc29e5c66bbd90dc244a5a60a
PrathmeshSarate <66374169+PrathmeshSarate@users.noreply.github.com>
2023-03-10 10:16:24 +0530

    first commit

---
+++ application/views/Web_Exontric/contact_us.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+    
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <!-- font-awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+
+    <!-- custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('/web_resources/css/style.css') ?>">
+
+
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+    <!-- font-awesome JS -->    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" integrity="sha512-2bMhOkE/ACz21dJT8zBOMgMecNxx0d37NND803ExktKiKdSzdwn+L7i9fdccw/3V06gM/DBWKbYmQvKMdAA9Nw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+
+    <div class="container">
+        <h1>HELLO</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ea suscipit nostrum cupiditate quae, minus repellat quidem facere error rerum et nisi minima corrupti aut officia aliquam unde exercitationem voluptatum! Non eum et, ea numquam, ex in modi magni, doloremque unde exercitationem sit! Quaerat neque officia enim ab animi exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ea suscipit nostrum cupiditate quae, minus repellat quidem facere error rerum et nisi minima corrupti aut officia aliquam unde exercitationem voluptatum! Non eum et, ea numquam, ex in modi magni, doloremque unde exercitationem sit! Quaerat neque officia enim ab animi exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ea suscipit nostrum cupiditate quae, minus repellat quidem facere error rerum et nisi minima corrupti aut officia aliquam unde exercitationem voluptatum! Non eum et, ea numquam, ex in modi magni, doloremque unde exercitationem sit! Quaerat neque officia enim ab animi exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ea suscipit nostrum cupiditate quae, minus repellat quidem facere error rerum et nisi minima corrupti aut officia aliquam unde exercitationem voluptatum! Non eum et, ea numquam, ex in modi magni, doloremque unde exercitationem sit! Quaerat neque officia enim ab animi exercitationem.</p>
+    </div>
+
+
+    <div class="container-fluid">
+        <footer>
+            <!-- FOLLOW SOCIAL ROW START -->
+                <div class="row">
+                    <div class="col-lg-6 footer-social-media">Follow us on 
+                        <i class="fa-brands fa-facebook"></i> 
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-linkedin"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+                            <img src="<?php echo base_url("/web_resources/img/footer-search-icon.svg") ?>" alt="footer-search-icon" class="footer-search-icon">
+                            <input type="text" placeholder="Search on site" name="search2">  
+                        </form>
+                    </div>
+
+                </div>
+            <!-- FOLLOW SOCIAL ROW END -->
+        </footer>
+    </div>
+
+</body>
+</html>
+<?php
+
+
+
+
+?>
